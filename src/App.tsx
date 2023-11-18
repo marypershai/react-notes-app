48d6a66a9e92b27feb0c86f883899f38c83c2e9f
Maryia Piarshai <mary.pershay@gmail.com>
2023-11-18 12:29:46 +0100

    feat: add simple structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,25 @@
-import {useState} from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import {Button} from './components/button/Button';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const newtest = 1;
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+    <div className="login-wrap">
+      <h2>Login</h2>
+      <div className="form">
+        <div className="form-field">
+          <input type="text" placeholder="Username" />
+          <p className="error-text">Please check</p>
+        </div>
+        <div className="form-field error">
+          <input type="password" placeholder="Password" />
+          <p className="error-text">Please check</p>
+        </div>
+        <Button text={'Submit'} />
+        <a href="#">
+          <p> Cancel </p>
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/button/Button.tsx
@@ -0,0 +1,5 @@
+import './Button.css';
+
+export const Button = ({text}) => {
+  return <button>{text}</button>;
+};
